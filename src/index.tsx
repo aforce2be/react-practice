373cd5bff9af0a9a5955b19886d6a08ccd6ea02b
Sujin Park <aforce2be@naver.com>
2026-03-11 22:55:03 +0900

    feat(product): React Query 기반 서버 상태 관리 적용 및 Devtools 설치

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './App'
 
 const queryClient = new QueryClient()
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 )
--- src/pages/product/ProductListPage.tsx
+++ src/pages/product/ProductListPage.tsx
@@ -1,7 +1,7 @@
 import { useProducts } from '../../hooks/useProducts'
 
 const ProductListPage = () => {
-  const { data, isLoading, isError } = useProducts()
+  const { data: products, isLoading, isError } = useProducts()
 
   if (isLoading) return <div>로딩중..</div>
   if (isError) return <div>에러 발생!</div>
@@ -10,7 +10,7 @@ const ProductListPage = () => {
     <div>
       <h2>상품 목록</h2>
       <ul>
-        {data?.map((product) => (
+        {products?.map((product) => (
           <li key={product.id}>
             {product.name}-{product.price.toLocaleString()}원
           </li>
